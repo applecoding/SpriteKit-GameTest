476fd0a8f531122e912d8a11abcde02ef0326808
Julio César Fernández <juliocesar.fernandez@icloud.com>
2020-12-30 20:59:24 +0100

    Fix

--- GameTest Shared/GameScene.swift
+++ GameTest Shared/GameScene.swift
@@ -9,6 +9,8 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var isMoving = false
+    
     class func newGameScene() -> GameScene {
         // Load 'GameScene.sks' as an SKScene.
         guard let scene = SKScene(fileNamed: "GameScene") as? GameScene else {
@@ -28,47 +30,53 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         self.setUpScene()
+        let gesture = UISwipeGestureRecognizer(target: self, action: #selector(salto(_:)))
+        gesture.direction = .up
+        self.view?.addGestureRecognizer(gesture)
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first,
               let aventura = self.childNode(withName: "Aventurer") as? SKSpriteNode,
               let advRun = SKAction(named: "AdvRun"),
-              let advJump = SKAction(named: "AdvJump") else {
+              !isMoving else {
             return
         }
         let position = touch.location(in: self)
-        if touch.tapCount == 1 {
-            if aventura.position.x > position.x {
-                aventura.xScale = -1
-            } else {
-                aventura.xScale = 1
-            }
-            let actionMove = SKAction.move(to: position, duration: 1)
-            actionMove.timingMode = .easeInEaseOut
-            let moveRun = SKAction.group([advRun, actionMove])
-            aventura.run(moveRun)
-        } else if touch.tapCount == 2 {
-            let saltoMov1 = SKAction.moveTo(y: aventura.position.y + 50, duration: 0.4)
-            let saltoMov2 = SKAction.moveTo(y: aventura.position.y - 50, duration: 0.4)
-            let secuenciaMov = SKAction.sequence([
-                saltoMov1, saltoMov2
-            ])
-            secuenciaMov.timingMode = .easeInEaseOut
-            let jump = SKAction.group([
-                advJump, secuenciaMov
-            ])
-            aventura.run(jump)
+        if aventura.position.x > position.x {
+            aventura.xScale = -1
+        } else {
+            aventura.xScale = 1
         }
+        let actionMove = SKAction.move(to: position, duration: 1)
+        actionMove.timingMode = .easeInEaseOut
+        let moveRun = SKAction.group([advRun, actionMove])
+        isMoving = true
+        aventura.run(moveRun) { self.isMoving = false }
     }
-    
+    @objc func salto(_ gesture:UISwipeGestureRecognizer) {
+        guard let aventura = self.childNode(withName: "Aventurer") as? SKSpriteNode,
+              let advJump = SKAction(named: "AdvJump") else {
+            return
+        }
+        let saltoMov1 = SKAction.moveTo(y: aventura.position.y + 50, duration: 0.4)
+        let saltoMov2 = SKAction.moveTo(y: aventura.position.y, duration: 0.4)
+        let secuenciaMov = SKAction.sequence([
+            saltoMov1, saltoMov2
+        ])
+        secuenciaMov.timingMode = .easeInEaseOut
+        let jump = SKAction.group([
+            advJump, secuenciaMov
+        ])
+        isMoving = true
+        aventura.run(jump) { self.isMoving = false }    }
 }
 
 #if os(OSX)
 
 // Mouse-based event handling
 extension GameScene {
-
+    
 }
 #endif
 
