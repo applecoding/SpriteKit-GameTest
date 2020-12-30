2578850e0a27f606acd9205f21618d7e74ea6713
Julio César Fernández <juliocesar.fernandez@icloud.com>
2020-12-30 20:42:27 +0100

    Día 2 OK

--- GameTest Shared/GameScene.swift
+++ GameTest Shared/GameScene.swift
@@ -29,6 +29,39 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         self.setUpScene()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first,
+              let aventura = self.childNode(withName: "Aventurer") as? SKSpriteNode,
+              let advRun = SKAction(named: "AdvRun"),
+              let advJump = SKAction(named: "AdvJump") else {
+            return
+        }
+        let position = touch.location(in: self)
+        if touch.tapCount == 1 {
+            if aventura.position.x > position.x {
+                aventura.xScale = -1
+            } else {
+                aventura.xScale = 1
+            }
+            let actionMove = SKAction.move(to: position, duration: 1)
+            actionMove.timingMode = .easeInEaseOut
+            let moveRun = SKAction.group([advRun, actionMove])
+            aventura.run(moveRun)
+        } else if touch.tapCount == 2 {
+            let saltoMov1 = SKAction.moveTo(y: aventura.position.y + 50, duration: 0.4)
+            let saltoMov2 = SKAction.moveTo(y: aventura.position.y - 50, duration: 0.4)
+            let secuenciaMov = SKAction.sequence([
+                saltoMov1, saltoMov2
+            ])
+            secuenciaMov.timingMode = .easeInEaseOut
+            let jump = SKAction.group([
+                advJump, secuenciaMov
+            ])
+            aventura.run(jump)
+        }
+    }
+    
 }
 
 #if os(OSX)
